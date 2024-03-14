acf471b44d26b16379f256980346202634bea962
DuyThin0207 <thinnguyen3030@gmail.com>
2024-03-14 11:00:10 +0700

    test pull

--- src/page/login/Register.js
+++ src/page/login/Register.js
@@ -1,6 +1,6 @@
 import {Link} from "react-router-dom";
 import {Field, Form, Formik} from "formik";
-import {useDispatch} from "react-redux";
+// import {useDispatch} from "react-redux";
 
 export default function Register() {
     return (
