28b0b0c7c8322e7a68a90b289e053c527be9f276
DuyThin0207 <thinnguyen3030@gmail.com>
2024-03-15 09:57:48 +0700

    git fe

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import "./page/login/login.css";
 import './App.css';
 import Wallet from "./page/wallet/showWallet/showWallet";
-// import {Route, Routes} from "react-router-dom";
-// import HomeLogin from "./page/login/HomeLogin";
-// import Login from "./page/login/Login";
-// import Register from "./page/login/Register";
-// import Home from "./page/home/Home";
-// import {useSelector} from "react-redux";
+import {Route, Routes} from "react-router-dom";
+import HomeLogin from "./page/login/HomeLogin";
+import Login from "./page/login/Login";
+import Register from "./page/login/Register";
+import Home from "./page/home/Home";
+import {useSelector} from "react-redux";
 function App() {
-    // const users = useSelector(state => {
-    //     console.log(state.users.users)
-    //     return state.users.users
-    // })
+    const users = useSelector(state => {
+        console.log(state.users.users)
+        return state.users.users
+    })
     return (
         <>
             <Wallet></Wallet>
-            {/*<Routes>*/}
-            {/*    <Route path={"/login"} element={<HomeLogin/>}>*/}
-            {/*        <Route path={''} element={<Login/>}></Route>*/}
-            {/*        <Route path={'register'} element={<Register/>}></Route>*/}
-            {/*    </Route>*/}
-            {/*    {*/}
-            {/*        users !== null ?*/}
-            {/*            <Route path={'/home'} element={<Home/>}>*/}
-            {/*            </Route> :*/}
-            {/*            <Route path={"/login"} element={<HomeLogin/>}></Route>*/}
+            <Routes>
+                <Route path={"/login"} element={<HomeLogin/>}>
+                    <Route path={''} element={<Login/>}></Route>
+                    <Route path={'register'} element={<Register/>}></Route>
+                </Route>
+                {
+                    users !== null ?
+                        <Route path={'/home'} element={<Home/>}>
+                        </Route> :
+                        <Route path={"/login"} element={<HomeLogin/>}></Route>
 
-            {/*    }*/}
-            {/*</Routes>*/}
+                }
+            </Routes>
         </>
     );
 }
