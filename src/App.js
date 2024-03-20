c10586dce86d20ce11e58ba6eafd0884f78c51ef
DuyThin0207 <thinnguyen3030@gmail.com>
2024-03-20 12:05:54 +0700

    done FE profile, form edit + BE show profile

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Home from "./page/home/Home";
 import {useSelector} from "react-redux";
 import AddWallet from "./page/wallet/addWallet/AddWallet";
 import Wallet from "./page/wallet/Wallet";
+import {Profiler} from "react";
+import Account from "./page/account/Account";
 function App() {
     const users = useSelector(state => {
         return state.users.users
@@ -25,6 +27,7 @@ function App() {
                         <Route path={'home'} element={<Home/>}>
                             <Route path={''} element={<Wallet/>}></Route>
                             <Route path={'add-wallets'} element={<AddWallet/>}></Route>
+                            <Route path={'profile'} element={<Account/>}></Route>
                         </Route> :
                         <Route path={"/login"} element={<HomeLogin/>}></Route>
                 }
