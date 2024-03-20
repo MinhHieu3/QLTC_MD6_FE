d2c5f092bc25b83a160ed816952647ca0793f563
MinhHieu3 <hieubyi@gmail.com>
2024-03-20 10:05:22 +0700

    redux id user

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,8 +1,9 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {getUsers} from "../../service/user/userService";
+import {findById, getUsers} from "../../service/user/userService";
 
 const initialState = {
-    users: JSON.parse(localStorage.getItem('user'))
+    users: JSON.parse(localStorage.getItem('user')),
+    usersById:[]
 }
 const userSlice = createSlice({
     name: 'users',
@@ -11,6 +12,9 @@ const userSlice = createSlice({
         builder.addCase(getUsers.fulfilled, (state, action) => {
             state.users = action.payload.data
             localStorage.setItem('user',JSON.stringify(action.payload.data))
+        })
+        builder.addCase(findById.fulfilled, (state, action) => {
+            state.usersById = action.payload.data
         })
 
     }
