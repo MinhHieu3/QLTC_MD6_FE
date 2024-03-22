bcaee4d38f5769afc13039d066c6df5f2345851b
DuyThin0207 <thinnguyen3030@gmail.com>
2024-03-22 14:09:58 +0700

    done show form edit pass, if

--- src/page/account/Account.js
+++ src/page/account/Account.js
@@ -1,38 +1,46 @@
 import "./Accounts.css"
-import {useEffect, useState} from "react";
-import {Formik, Form, Field} from 'formik';
-import {useDispatch, useSelector} from "react-redux";
-import {findById} from "../../service/user/userService";
-import {Link, useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Formik, Form, Field } from 'formik';
+import { useDispatch, useSelector } from "react-redux";
+import { findById } from "../../service/user/userService";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Account() {
+    const [showUpdateInfo, setShowUpdateInfo] = useState(false);
+    const [showUpdatePassword, setShowUpdatePassword] = useState(false);
     const [showUpdateTable, setShowUpdateTable] = useState(false);
     const dispatch = useDispatch();
 
-    const handleUpdateClick = () => {
-        setShowUpdateTable(!showUpdateTable);
+    const handleUpdateInfoClick = () => {
+        setShowUpdateInfo(true);
+        setShowUpdatePassword(false);
+        setShowUpdateTable(true);
     };
-    const users = useSelector(state => {
-        return state.users.users
-    })
 
-    const user = useSelector(state => {
-        console.log("user", state.users.usersById)
-        return state.users.usersById
-    })
+    const handleUpdatePasswordClick = () => {
+        setShowUpdateInfo(false);
+        setShowUpdatePassword(true);
+        setShowUpdateTable(true);
+    };
+
+    const users = useSelector(state => state.users.users);
+
+    const user = useSelector(state => state.users.usersById);
 
     useEffect(() => {
         dispatch(findById(users.id))
     }, [])
+
     const oldUserData = {
-        // avatar: user.avatar,
         name: user.name,
         phone: user.phone,
-        password:users.password
+        password: user.password
     };
-    const handleSubmit = (values) => {
 
+    const handleSubmit = (values) => {
+        // Xử lý submit form
     }
+
     return (
         <>
             <div className="flex-container-account border-wallets">
@@ -57,13 +65,17 @@ export default function Account() {
                                  alt={''}
                             />
                         </div>
-                        <div className="child-div profile-account-center" style={{minWidth: 250}}>
+                        <div className="child-div profile-account-center" style={{ minWidth: 250 }}>
                             <div>Username: {users.username}</div>
                             <div className="text-devices">Name: {user.name}</div>
                             <div className="text-devices">Phone: {user.phone}</div>
                         </div>
-                        <div className="child-div profile-account-center">
-                            <button className="btn btn-secondary" onClick={handleUpdateClick}>Update</button>
+                        <div className="dropdown">
+                            <div className="dropbtn"> Update</div>
+                            <div className="dropdown-content">
+                                <div onClick={handleUpdateInfoClick}> check</div>
+                                <div onClick={handleUpdatePasswordClick}> check</div>
+                            </div>
                         </div>
                         <Formik
                             initialValues={oldUserData}
@@ -75,27 +87,75 @@ export default function Account() {
                             <Form>
                                 {showUpdateTable && (
                                     <div>
-                                        <div className={"border-wallets update-profile-table"}>
-                                            <div className="form-group-edit-account">
-                                                <label htmlFor="avatarInput">Avatar:</label>
-                                                <img src={user.avatar} alt="Avatar" className="img-edit-account" />
-                                                <Field type="file" name="avatar" className="form-control-file"
-                                                       id="avatarInput"/>
-                                            </div>
-                                            <div className="form-group-edit-account">
-                                                <label htmlFor="name">Name:</label>
-                                                <Field type="text" id="name" name="name" className="form-control"
-                                                       placeholder="Enter name"/>
-                                            </div>
-                                            <div className="form-group-edit-account">
-                                                <label htmlFor="phone">Phone:</label>
-                                                <Field type="number" id="phone" name="phone" className="form-control"
-                                                       placeholder="Enter phone"/>
-                                            </div>
-                                            <div className={"close-update-account"}>
-                                                <button type="submit" className="btn btn-secondary ">Submit</button>
-                                            </div>
-                                        </div>
+                                            {showUpdateInfo && (
+                                                <div className={"border-wallets update-profile-table"}>
+                                                    <div className="form-group-edit-account close-show-if" onClick={()=>{
+                                                        setShowUpdateInfo(false)
+                                                    }}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                                             className="bi bi-x-lg" viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                                                        </svg>
+                                                    </div>
+                                                    <div className="form-group-edit-account">
+                                                        <label htmlFor="avatarInput">Avatar:</label>
+                                                        <img src={user.avatar} alt="Avatar" className="img-edit-account" />
+                                                        <Field type="file" name="avatar" className="form-control-file"
+                                                               id="avatarInput" />
+                                                    </div>
+                                                    <div className="form-group-edit-account">
+                                                        <label htmlFor="name">Name:</label>
+                                                        <Field type="text" id="name" name="name" className="form-control"
+                                                               placeholder="Enter name" />
+                                                    </div>
+                                                    <div className="form-group-edit-account">
+                                                        <label htmlFor="phone">Phone:</label>
+                                                        <Field type="number" id="phone" name="phone" className="form-control"
+                                                               placeholder="Enter phone" />
+                                                    </div>
+                                                    <div className={"close-update-account"}>
+                                                        <button type="submit" className="btn btn-secondary ">Submit</button>
+                                                    </div>
+                                                </div>
+                                            )}
+                                            {showUpdatePassword && (
+                                                <div className={"border-wallets update-profile-table"}>
+                                                    <div className="form-group-edit-account close-show-if" onClick={()=>{
+                                                        setShowUpdatePassword(false)
+                                                    }}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                                             className="bi bi-x-lg" viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                                                        </svg>
+                                                    </div>
+                                                    <div className="form-group-edit-account">
+                                                    <label htmlFor="password">Password:</label>
+                                                    <Field type="password" id="password" name="password"
+                                                           className="form-control"
+                                                           placeholder="Enter password"/>
+                                                </div>
+                                                    <div className="form-group-edit-account">
+                                                    <label htmlFor="password">Password:</label>
+                                                    <Field type="password" id="password" name="password"
+                                                           className="form-control"
+                                                           placeholder="Enter password"/>
+                                                </div>
+                                                    <div className="form-group-edit-account">
+                                                    <label htmlFor="password">Password:</label>
+                                                    <Field type="password" id="password" name="password"
+                                                           className="form-control"
+                                                           placeholder="Enter password"/>
+                                                </div>
+                                                    <div className={"close-update-account"}>
+                                                        <button type="submit" className="btn btn-secondary ">Submit</button>
+                                                    </div>
+                                                </div>
+
+                                            )}
+
+
                                     </div>
                                 )}
                             </Form>
@@ -103,7 +163,7 @@ export default function Account() {
                     </div>
                     <div className={"main-account-top3"}>
                         <div className={"list-devices"}>Devices (1/5)</div>
-                        <div style={{display: "flex"}}>
+                        <div style={{ display: "flex" }}>
                             <div className="icon-container">
                                 <div className="icon"></div>
                             </div>
@@ -112,7 +172,6 @@ export default function Account() {
                                 <div className={"text-devices"}>This device</div>
                             </div>
                         </div>
-
                     </div>
                 </div>
                 <div className={"btn-account"}>
