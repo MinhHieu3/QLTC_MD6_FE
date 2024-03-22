28368644f07299807c921fdce8f2893d43b114c5
MinhHieu3 <hieubyi@gmail.com>
2024-03-22 11:19:02 +0700

    merge

--- src/page/account/Account.js
+++ src/page/account/Account.js
@@ -3,63 +3,55 @@ import {useEffect, useState} from "react";
 import {Formik, Form, Field} from 'formik';
 import {useDispatch, useSelector} from "react-redux";
 import {findById} from "../../service/user/userService";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Account() {
     const [showUpdateTable, setShowUpdateTable] = useState(false);
     const dispatch = useDispatch();
 
     const handleUpdateClick = () => {
-        setShowUpdateTable(true);
+        setShowUpdateTable(!showUpdateTable);
     };
     const users = useSelector(state => {
         return state.users.users
     })
 
     const user = useSelector(state => {
-        console.log("user",state.users.usersById)
+        console.log("user", state.users.usersById)
         return state.users.usersById
     })
-    useEffect(()=> {
+    useEffect(() => {
         dispatch(findById(users.id))
-    },[])
+    }, [])
 
-    const oldUserData = {
-        avatar: user.avatar,
-        name: user.name,
-        phone: user.phone,
-        password:users.password
-    };
-    const handleSubmit=(values)=>{
+    const handleSubmit = (values) => {
 
     }
     return (
         <>
             <div className="flex-container-account border-wallets">
-                <div style={{color: "black"}}>
-                    <div className={"title-account"}>
-                        <div className={"title-account-center"}>My Account</div>
-                        <div className={"close-title-account"}>
-                            <a href="/home" style={{color: "black"}}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                     className="bi bi-x-lg" viewBox="0 0 16 16">
-                                    <path
-                                        d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                                </svg>
-                            </a>
-                        </div>
+                <div className="title-account">
+                    <div className="title-account-center">My Account</div>
+                    <div className="close-title-account">
+                        <Link to="/home">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path
+                                    d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                            </svg>
+                        </Link>
                     </div>
-                    <hr/>
                 </div>
+                <hr className="hr" />
                 <div>
                     <div className="main-account-top1">
                         <div className="child-div">
                             <img className="img-profile-account"
-                                 src="https://th.bing.com/th/id/R.c11b6f38dffc24a4508217513b0e50bd?rik=Pt%2bkITlukiMkWg&riu=http%3a%2f%2fwww.emmegi.co.uk%2fwp-content%2fuploads%2f2019%2f01%2fUser-Icon.jpg&ehk=zjS04fF4nxx%2bpkvRPsSezyic3Z7Yfu%2fuoT75KnbNv1Y%3d&risl=&pid=ImgRaw&r=0"
+                                 src={users.avatar}
                                  alt=""
                             />
                         </div>
-                        <div className="child-div profile-account-center" style={{minWidth:250}}>
+                        <div className="child-div profile-account-center" style={{minWidth: 250}}>
                             <div>Username: {users.username}</div>
                             <div className="text-devices">Name: {user.name}</div>
                             <div className="text-devices">Phone: {user.phone}</div>
@@ -68,8 +60,8 @@ export default function Account() {
                             <button className="btn btn-secondary" onClick={handleUpdateClick}>Update</button>
                         </div>
                         <Formik
-                            initialValues={oldUserData}
-                            onSubmit={(values)=>{
+                            initialValues={users}
+                            onSubmit={(values) => {
                                 handleSubmit(values)
                             }}
                             enableReinitialize={true}
@@ -78,30 +70,20 @@ export default function Account() {
                                 {showUpdateTable && (
                                     <div>
                                         <div className={"border-wallets update-profile-table"}>
-                                            <div className={"close-update-account"}>
-                                                <a href="/home/profile" style={{color: "black"}}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                                         className="bi bi-x-lg" viewBox="0 0 16 16">
-                                                        <path
-                                                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                                                    </svg>
-                                                </a>
-                                            </div>
                                             <div className="form-group-edit-account">
                                                 <label htmlFor="avatarInput">Avatar:</label>
-                                                <Field type="file" name="avatar" className="form-control-file" id="avatarInput"/>
+                                                <Field type="file" name="avatar" className="form-control-file"
+                                                       id="avatarInput"/>
                                             </div>
                                             <div className="form-group-edit-account">
                                                 <label htmlFor="name">Name:</label>
-                                                <Field type="text" id="name" name="name" className="form-control" placeholder="Enter name"/>
+                                                <Field type="text" id="name" name="name" className="form-control"
+                                                       placeholder="Enter name"/>
                                             </div>
                                             <div className="form-group-edit-account">
-                                            <label htmlFor="phone">Phone:</label>
-                                                <Field type="number" id="phone" name="phone" className="form-control" placeholder="Enter phone"/>
-                                            </div>
-                                            <div className="form-group-edit-account">
-                                                <label htmlFor="password">Pass word:</label>
-                                                <Field type="password" id="phone" name="password" className="form-control" placeholder="password"/>
+                                                <label htmlFor="phone">Phone:</label>
+                                                <Field type="number" id="phone" name="phone" className="form-control"
+                                                       placeholder="Enter phone"/>
                                             </div>
                                             <div className={"close-update-account"}>
                                                 <button type="submit" className="btn btn-secondary ">Submit</button>
@@ -126,11 +108,11 @@ export default function Account() {
 
                     </div>
                 </div>
-                <div style={{marginLeft: "auto", marginTop: "auto"}}>
+                <div className={"btn-account"}>
                     <a href="/login">
                         <button className={"btn btn-secondary sing-out-account-btn"}>Sign out</button>
                     </a>
-                    <button className={"btn btn-danger"}>Delete Account</button>
+                    <button className={"btn btn-danger"}>Delete</button>
                 </div>
             </div>
         </>
